1d3d0f854a3861c31632e5d7e1372a732284d4b7
Jony <jonyreiscardoso@gmail.com>
2020-10-17 17:26:31 -0400

    Refatorando a Task.Utils.AddFakeFriends

--- friends_app/lib/mix/tasks/utils/add_fake_friends.ex
+++ friends_app/lib/mix/tasks/utils/add_fake_friends.ex
@@ -22,7 +22,13 @@ defmodule Mix.Tasks.Utils.AddFakeFriends do
     end
 
     defp random_list_friend do
-        [ Faker.Person.PtBr.name, Faker.Internet.email(), Faker.Phone.EnUs.phone() ]
+        # [ Faker.Person.PtBr.name, Faker.Internet.email(), Faker.Phone.EnUs.phone() ]
+        %{
+            name: Faker.Person.PtBr.name,
+            email: Faker.Internet.email(),
+            phone: Faker.Phone.EnUs.phone()
+        }
+        |> Map.values
     end
 
     defp save_csv_file(data) do
